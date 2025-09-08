634508f3e829f7335e085107b067dbbf10532eba
avremy <avremy.back@norman-ai.com>
2025-09-08 21:54:04 +0300

    Refactor: fix typos in `socket_client` and update type validations in `persist` services for consistency.
    (#86c50m67k)

--- norman_core/services/retrieve/retrieve.py
+++ norman_core/services/retrieve/retrieve.py
@@ -7,14 +7,14 @@ class Retrieve:
     @staticmethod
     async def get_model_asset(http_client: HttpClient, token: Sensitive[str], account_id: str, model_id: str, asset_id: str):
         endpoint = f"retrieve/asset/{account_id}/{model_id}/{asset_id}"
-        return await http_client.stream("GET", endpoint, token)
+        return await http_client.get(endpoint, token, response_encoding=ResponseEncoding.Iterator)
 
     @staticmethod
     async def get_invocation_input(http_client: HttpClient, token: Sensitive[str], account_id: str, model_id: str, invocation_id: str, input_id: str):
         endpoint = f"retrieve/input/{account_id}/{model_id}/{invocation_id}/{input_id}"
-        return await http_client.stream("GET", endpoint, token)
+        return await http_client.get(endpoint, token, response_encoding=ResponseEncoding.Iterator)
 
     @staticmethod
     async def get_invocation_output(http_client: HttpClient, token: Sensitive[str], account_id: str, model_id: str, invocation_id: str, output_id: str):
         endpoint = f"retrieve/output/{account_id}/{model_id}/{invocation_id}/{output_id}"
-        return await http_client.stream("GET", endpoint, token)
+        return await http_client.get(endpoint, token, response_encoding=ResponseEncoding.Iterator)
