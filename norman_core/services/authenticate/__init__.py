41a4e4bf401724569849bc3012fbfa3bc0d900ff
Dolev <dolev.gabay@norman-ai.com>
2025-10-21 17:43:05 +0300

    Make http client singleton and all the services as well.
    (#86c4zmpcp)

--- norman_core/services/authenticate/__init__.py
+++ norman_core/services/authenticate/__init__.py
@@ -1,13 +1,17 @@
+from norman_utils_external.singleton import Singleton
+
 from norman_core.services.authenticate.accounts import Accounts
 from norman_core.services.authenticate.login import Login
 from norman_core.services.authenticate.register import Register
 from norman_core.services.authenticate.signup import Signup
 
 
-class Authenticate:
-    accounts = Accounts
-    login = Login
-    register = Register
-    signup = Signup
+class Authenticate(metaclass=Singleton):
+    def __init__(self):
+        self.accounts = Accounts()
+        self.login = Login()
+        self.register = Register()
+        self.signup = Signup()
 
-__all__ = ['Authenticate']
+
+__all__ = ["Authenticate"]
