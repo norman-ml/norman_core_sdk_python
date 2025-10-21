69612725cb01f963f095eb6e15e593132fe3ea7b
Dolev <dolev.gabay@norman-ai.com>
2025-10-21 18:00:23 +0300

    Make http client singleton and all the services as well.
    (#86c4zmpcp)

--- norman_core/services/persist/__init__.py
+++ norman_core/services/persist/__init__.py
@@ -1,3 +1,5 @@
+from norman_utils_external.singleton import Singleton
+
 from norman_core.services.persist.invocations import Invocations
 from norman_core.services.persist.model_bases import ModelBases
 from norman_core.services.persist.models import Models
@@ -5,12 +7,12 @@ from norman_core.services.persist.notifications import Notifications
 from norman_core.services.persist.status_flags import StatusFlags
 
 
-class Persist:
-    invocations = Invocations
-    model_bases = ModelBases
-    models = Models
-    notifications = Notifications
-    status_flags = StatusFlags
-
+class Persist(metaclass=Singleton):
+    def __init__(self):
+        self.invocations = Invocations()
+        self.model_bases = ModelBases()
+        self.models = Models()
+        self.notifications = Notifications()
+        self.status_flags = StatusFlags()
 
 __all__ = ["Persist"]
