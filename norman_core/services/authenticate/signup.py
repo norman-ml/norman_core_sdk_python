7aaf7846d75d5d296c5211563df579dd4944dc2c
Dolev <dolev.gabay@norman-ai.com>
2025-10-25 15:53:12 +0300

    Change the signup to signup with api key.
    (#86c4p1j6d)

--- norman_core/services/authenticate/signup.py
+++ norman_core/services/authenticate/signup.py
@@ -1,9 +1,10 @@
 from norman_objects.services.authenticate.login.login_response import LoginResponse
 from norman_objects.services.authenticate.signup.signup_email_request import SignupEmailRequest
+from norman_objects.services.authenticate.signup.signup_password_request import SignupKeyRequest
 from norman_objects.services.authenticate.signup.signup_password_request import SignupPasswordRequest
 from norman_objects.shared.accounts.account import Account
-
 from norman_utils_external.singleton import Singleton
+
 from norman_core.clients.http_client import HttpClient
 
 
@@ -24,3 +25,8 @@ class Signup(metaclass=Singleton):
         json = signup_request.model_dump(mode="json")
         response = await self._http_client.put("authenticate/signup/email", json=json)
         return Account.model_validate(response)
+
+    async def signup_and_generate_key(self, signup_request: SignupKeyRequest) -> Account:
+        json = signup_request.model_dump(mode="json")
+        response = await self._http_client.put("authenticate/signup/key", json=json)
+        return response
