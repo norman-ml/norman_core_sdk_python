5b1bab04c347c6fe56ac2b8cfe179671a922f97d
NogaUnger <noga.unger@norman-ai.com>
2025-12-04 00:01:11 +0200

    Fixing a bug in the requirements.txt and also changing get_model_bases func to match the changes made in persist (using constraints).

--- norman_core/services/persist/model_bases.py
+++ norman_core/services/persist/model_bases.py
@@ -1,11 +1,11 @@
 from typing import Optional
 
 from norman_objects.shared.models.model_base import ModelBase
-from norman_objects.shared.requests.get_models_request import GetModelsRequest
+from norman_objects.shared.queries.query_constraints import QueryConstraints
 from norman_objects.shared.security.sensitive import Sensitive
+from norman_utils_external.singleton import Singleton
 from pydantic import TypeAdapter
 
-from norman_utils_external.singleton import Singleton
 from norman_core.clients.http_client import HttpClient
 
 
@@ -13,10 +13,7 @@ class ModelBases(metaclass=Singleton):
     def __init__(self) -> None:
         self._http_client = HttpClient()
 
-    async def get_model_bases(self, token: Sensitive[str], request: Optional[GetModelsRequest] = None) -> dict[str, ModelBase]:
-        if request is None:
-            request = GetModelsRequest(constraints=None, finished_models=True)
-        json = request.model_dump(mode="json")
-
+    async def get_model_bases(self, token: Sensitive[str], constraint: Optional[QueryConstraints] = None) -> dict[str, ModelBase]:
+        json = constraint.model_dump(mode="json")
         response = await self._http_client.post("persist/models/bases/get", token, json=json)
         return TypeAdapter(dict[str, ModelBase]).validate_python(response)
