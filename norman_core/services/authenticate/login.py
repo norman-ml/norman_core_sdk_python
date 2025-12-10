e32359741a54a03275ca8fad984c513f66c385c3
NogaUnger <noga.unger@norman-ai.com>
2025-12-10 18:20:42 +0200

    Inst=erting a whitespace so that we dont have additional file stam.

--- norman_core/services/authenticate/login.py
+++ norman_core/services/authenticate/login.py
@@ -3,6 +3,7 @@ from norman_objects.services.authenticate.login.api_key_login_request import Api
 from norman_objects.services.authenticate.login.email_password_login_request import EmailPasswordLoginRequest
 from norman_objects.services.authenticate.login.login_response import LoginResponse
 from norman_objects.services.authenticate.login.name_password_login_request import NamePasswordLoginRequest
+
 from norman_utils_external.singleton import Singleton
 from norman_core.clients.http_client import HttpClient
 
