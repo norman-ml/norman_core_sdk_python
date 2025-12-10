7bd80587dd5f5c0ac2cd353b7088e13803684dc2
AmitMizrachi <amit.mizrachi@norman-ai.com>
2025-12-10 14:49:37 +0200

    Adding norman-hug to core sdk, with download_huggingface_model function.

---
+++ norman_core/services/hug/hug.py
@@ -0,0 +1,19 @@
+from norman_objects.services.file_push.checksum.checksum_request import ChecksumRequest
+from norman_objects.services.file_push.pairing.socket_input_pairing_request import SocketInputPairingRequest
+from norman_objects.services.file_push.pairing.socket_pairing_response import SocketPairingResponse
+from norman_objects.services.hug.huggingface_download_request import HuggingFaceDownloadRequest
+from norman_objects.shared.security.sensitive import Sensitive
+from norman_utils_external.singleton import Singleton
+from pydantic import TypeAdapter
+
+from norman_core.clients.http_client import HttpClient
+
+
+class Hug(metaclass=Singleton):
+    def __init__(self) -> None:
+        self._http_client = HttpClient()
+
+    async def download_huggingface_model(self, token: Sensitive[str], huggingface_download_request: HuggingFaceDownloadRequest)  -> list[str]:
+        json = huggingface_download_request.model_dump(mode="json")
+        response = await self._http_client.post("hug/download", token, json=json)
+        return TypeAdapter(list[str]).validate_python(response)
