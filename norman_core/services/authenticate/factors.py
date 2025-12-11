e88fa1f9ba1d7d55bf560a5047050c0b01bf6444
Yonatan Alon <yonatan.alon@norman-ai.com>
2025-12-11 11:58:28 +0200

    Exposed new authentication factor get methods through our sdk export mechanisms.
    (#86c6af6un)

--- norman_core/services/authenticate/factors.py
+++ norman_core/services/authenticate/factors.py
@@ -1,14 +1,21 @@
 from norman_objects.shared.authentication.account_authentication_methods import AccountAuthenticationMethods
+from norman_utils_external.singleton import Singleton
+
+from norman_core.clients.http_client import HttpClient
 
 
-async def get_authentication_factors_by_id(self, account_id: str) -> AccountAuthenticationMethods:
-    response = await self._http_client.get(f"authenticate/login/get/authentication/factors/by-id/{account_id}")
-    return AccountAuthenticationMethods.model_validate(response)
+class Factors(metaclass=Singleton):
+    def __init__(self) -> None:
+        self._http_client = HttpClient()
 
-async def get_authentication_factors_by_name(self, account_name: str) -> AccountAuthenticationMethods:
-    response = await self._http_client.get(f"authenticate/login/get/authentication/factors/by-name/{account_name}")
-    return AccountAuthenticationMethods.model_validate(response)
+    async def get_authentication_factors_by_id(self, account_id: str) -> AccountAuthenticationMethods:
+        response = await self._http_client.get(f"authenticate/login/get/authentication/factors/by-id/{account_id}")
+        return AccountAuthenticationMethods.model_validate(response)
 
-async def get_authentication_factors_by_email(self, email: str) -> AccountAuthenticationMethods:
-    response = await self._http_client.get(f"authenticate/login/get/authentication/factors/by-email/{email}")
-    return AccountAuthenticationMethods.model_validate(response)
+    async def get_authentication_factors_by_name(self, account_name: str) -> AccountAuthenticationMethods:
+        response = await self._http_client.get(f"authenticate/login/get/authentication/factors/by-name/{account_name}")
+        return AccountAuthenticationMethods.model_validate(response)
+
+    async def get_authentication_factors_by_email(self, email: str) -> AccountAuthenticationMethods:
+        response = await self._http_client.get(f"authenticate/login/get/authentication/factors/by-email/{email}")
+        return AccountAuthenticationMethods.model_validate(response)
