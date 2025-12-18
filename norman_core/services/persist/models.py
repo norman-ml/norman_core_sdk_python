6acd3d0fb56ba3ec7767c1ffffdec041d5320ace
Yonatan Alon <yonatan.alon@norman-ai.com>
2025-12-19 01:56:09 +0200

    Added model projection creation route to models route class.
    (#86c6wa00a)

--- norman_core/services/persist/models.py
+++ norman_core/services/persist/models.py
@@ -15,7 +15,7 @@ from norman_core.clients.http_client import HttpClient
 class Models(metaclass=Singleton):
     def __init__(self) -> None:
         self._http_client = HttpClient()
-    
+
     async def get_models(self, token: Sensitive[str], constraint: Optional[QueryConstraints] = None) -> dict[str, Model]:
         json = constraint.model_dump(mode="json")
         response = await self._http_client.post("persist/models/get", token, json=json)
@@ -25,7 +25,7 @@ class Models(metaclass=Singleton):
         json = constraint.model_dump(mode="json")
         response = await self._http_client.post("persist/models/previews/get", token, json=json)
         return TypeAdapter(dict[str, ModelPreview]).validate_python(response)
-    
+
     async def create_models(self, token: Sensitive[str], models: list[Model]) -> dict[str, Model]:
         json = None
         if models is not None:
@@ -34,6 +34,14 @@ class Models(metaclass=Singleton):
         response = await self._http_client.post("persist/models", token, json=json)
         return TypeAdapter(list[Model]).validate_python(response)
 
+    async def create_model_projections(self, token: Sensitive[str], models: list[ModelProjection]) -> dict[str, ModelProjection]:
+        json = None
+        if models is not None:
+            json = TypeAdapter(list[ModelProjection]).dump_python(models, mode="json")
+
+        response = await self._http_client.put("persist/models/projections", token, json=json)
+        return TypeAdapter(list[ModelProjection]).validate_python(response)
+
     async def upgrade_model_projections(self, token: Sensitive[str], model_projections: list[ModelProjection]) -> dict[str, ModelProjection]:
         json = None
         if model_projections is not None:
@@ -41,7 +49,7 @@ class Models(metaclass=Singleton):
 
         response = await self._http_client.post("persist/models/projections", token, json=json)
         return TypeAdapter(list[Model]).validate_python(response)
-    
+
     async def set_active_model_versions(self, token: Sensitive[str], model_version_previews: list[ModelVersionPreview]) -> list[ModelVersionPreview]:
         json = None
         if model_version_previews is not None:
@@ -49,7 +57,7 @@ class Models(metaclass=Singleton):
 
         response = await self._http_client.patch("persist/models/version", token, json=json)
         return TypeAdapter(list[ModelVersionPreview]).validate_python(response)
-    
+
     async def delete_models(self, token: Sensitive[str], constraints: QueryConstraints) -> int:
         json = constraints.model_dump()
 
