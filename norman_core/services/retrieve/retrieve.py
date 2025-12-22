b5c5627b791b20b71e280c69ac828a2a8208a1cf
AmitMizrachi <amit.mizrachi@norman-ai.com>
2025-12-22 22:07:35 +0200

    Changing model_id to version_id to match with the new s3 upload path.

--- norman_core/services/retrieve/retrieve.py
+++ norman_core/services/retrieve/retrieve.py
@@ -10,14 +10,14 @@ class Retrieve(metaclass=Singleton):
     def __init__(self) -> None:
         self._http_client = HttpClient()
 
-    async def get_model_asset(self, token: Sensitive[str], account_id: str, model_id: str, asset_id: str) -> Any:
-        endpoint = f"retrieve/asset/{account_id}/{model_id}/{asset_id}"
+    async def get_model_asset(self, token: Sensitive[str], account_id: str, version_id: str, asset_id: str) -> Any:
+        endpoint = f"retrieve/asset/{account_id}/{version_id}/{asset_id}"
         return await self._http_client.get(endpoint, token, response_encoding=ResponseEncoding.Iterator)
 
-    async def get_invocation_input(self, token: Sensitive[str], account_id: str, model_id: str, invocation_id: str, input_id: str) -> Any:
-        endpoint = f"retrieve/input/{account_id}/{model_id}/{invocation_id}/{input_id}"
+    async def get_invocation_input(self, token: Sensitive[str], account_id: str, version_id: str, invocation_id: str, input_id: str) -> Any:
+        endpoint = f"retrieve/input/{account_id}/{version_id}/{invocation_id}/{input_id}"
         return await self._http_client.get(endpoint, token, response_encoding=ResponseEncoding.Iterator)
 
-    async def get_invocation_output(self, token: Sensitive[str], account_id: str, model_id: str, invocation_id: str, output_id: str) -> Any:
-        endpoint = f"retrieve/output/{account_id}/{model_id}/{invocation_id}/{output_id}"
+    async def get_invocation_output(self, token: Sensitive[str], account_id: str, version_id: str, invocation_id: str, output_id: str) -> Any:
+        endpoint = f"retrieve/output/{account_id}/{version_id}/{invocation_id}/{output_id}"
         return await self._http_client.get(endpoint, token, response_encoding=ResponseEncoding.Iterator)
