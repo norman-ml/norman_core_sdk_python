205fc1770fdc98618319e3cbb9977ae91ae982d9
Yonatan Alon <yonatan.alon@norman-ai.com>
2026-01-01 01:49:21 +0200

    Added missing get download metadata route to norman hug service class.
    (#86c6wa00a)

--- norman_core/services/hug/hug.py
+++ norman_core/services/hug/hug.py
@@ -1,7 +1,5 @@
-from norman_objects.services.file_push.checksum.checksum_request import ChecksumRequest
-from norman_objects.services.file_push.pairing.socket_input_pairing_request import SocketInputPairingRequest
-from norman_objects.services.file_push.pairing.socket_pairing_response import SocketPairingResponse
 from norman_objects.services.hug.huggingface_download_request import HuggingFaceDownloadRequest
+from norman_objects.services.hug.tracked_huggingface_download import TrackedHuggingFaceDownload
 from norman_objects.shared.security.sensitive import Sensitive
 from norman_utils_external.singleton import Singleton
 from pydantic import TypeAdapter
@@ -12,6 +10,10 @@ from norman_core.clients.http_client import HttpClient
 class Hug(metaclass=Singleton):
     def __init__(self) -> None:
         self._http_client = HttpClient()
+
+    async def get_download_metadata(self, token: Sensitive[str], entity_id: str) -> TrackedHuggingFaceDownload:
+        response = await self._http_client.get(f"hug/metadata/{entity_id}", token)
+        return TypeAdapter(TrackedHuggingFaceDownload).validate_python(response)
 
     async def download_huggingface_model(self, token: Sensitive[str], download_request: HuggingFaceDownloadRequest)  -> list[str]:
         json = download_request.model_dump(mode="json")
