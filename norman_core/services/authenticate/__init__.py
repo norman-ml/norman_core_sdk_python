b6dceb94c83a32051b2e64458bd80522282a153c
AmitMizrachi <amit.mizrachi@norman-ai.com>
2026-01-01 10:19:20 +0200

    Adding jwks service to authenticate, and updating pyproject.toml with the correct branch pointers.

--- norman_core/services/authenticate/__init__.py
+++ norman_core/services/authenticate/__init__.py
@@ -2,6 +2,7 @@ from norman_utils_external.singleton import Singleton
 
 from norman_core.services.authenticate.accounts import Accounts
 from norman_core.services.authenticate.factors import Factors
+from norman_core.services.authenticate.jwks import JWKS
 from norman_core.services.authenticate.login import Login
 from norman_core.services.authenticate.logout import Logout
 from norman_core.services.authenticate.register import Register
@@ -12,6 +13,7 @@ class Authenticate(metaclass=Singleton):
     def __init__(self) -> None:
         self.accounts = Accounts()
         self.factors = Factors()
+        self.jwks = JWKS()
         self.login = Login()
         self.logout = Logout()
         self.register = Register()
---
+++ norman_core/services/authenticate/jwks.py
@@ -0,0 +1,13 @@
+from norman_objects.shared.authorization.jwk import JWK
+from norman_utils_external.singleton import Singleton
+
+from norman_core.clients.http_client import HttpClient
+
+
+class JWKS(metaclass=Singleton):
+    def __init__(self) -> None:
+        self._http_client = HttpClient()
+
+    async def get_jwks(self) -> list[JWK]:
+        response = await self._http_client.get("authenticate/jwks")
+        return [JWK.model_validate(key) for key in response["keys"]]
